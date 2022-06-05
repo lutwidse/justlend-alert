112616fc6fe4a9ccdace024643c06ca31d2044cb
Lutwidse <74334594+Lutwidse@users.noreply.github.com>
2022-06-06 06:02:38 +0900

    add usdd CR check

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from justlend import JustLend
+from tdr import TDR
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 import schedule
@@ -6,19 +7,24 @@ import time
 import os
 
 RISK_ALERT_THRESHOLD = 0.015
+USDD_CR_THRESHOLD = 0.1
+
 RISK_ALERT_DIGIT = int(len(str(RISK_ALERT_THRESHOLD).replace(".", "")))
+USDD_CR_DIGIT = int(len(str(USDD_CR_THRESHOLD).replace(".", "")))
 
 HELP_TEXT = (
     "/risk_check - Get current Risk-value. \n"
-    "/risk_alert_set - Set /risk_check notification if risk ups than the threshold. \n"
-    "/risk_alert_unset - Unset /risk_alert_set"
+    "/risk_alert_set - Set|Unset /risk_check notification if risk ups than the threshold. \n"
 )
 
 
 class TeleBot:
     def __init__(self, addr):
         self._just = JustLend(addr)
-        self.last_checked_risk_value = self._just.get_risk_value()
+        self._tdr = TDR()
+
+        self._last_checked_risk_value = self._just.get_risk_value()
+        self._last_checked_usdd_cr_value = self._tdr.get_collateralization_ratio()
 
     def remove_job_if_exists(self, name: str, context: CallbackContext) -> bool:
         current_jobs = context.job_queue.get_jobs_by_name(name)
@@ -28,10 +34,10 @@ class TeleBot:
             job.schedule_removal()
             return True
 
-    def start(self, update: Update, context: CallbackContext) -> None:
+    def start(self, update: Update, context: CallbackContext):
         update.message.reply_text(text="/help to see commands.")
 
-    def help(self, update: Update, context: CallbackContext) -> None:
+    def help(self, update: Update, context: CallbackContext):
         update.message.reply_text(text=HELP_TEXT)
 
     # Check risk value
@@ -42,16 +48,16 @@ class TeleBot:
         diff = self._last_checked_risk_value - self._just.get_risk_value()
         diff = round(diff, RISK_ALERT_DIGIT)
         # TODO: Calculate minus or plus
-        if RISK_ALERT_THRESHOLD <= abs(risk_diff):
+        if RISK_ALERT_THRESHOLD <= abs(diff):
             job = context.job
             context.bot.send_message(
                 job.context,
                 text="Risk Alert\n"
-                + f"[{self.last_checked_risk_value*100}] -> [{self._just.get_risk_value()*100}]"
+                + f"[{self._last_checked_risk_value*100}%] -> [{self._just.get_risk_value()*100}%]"
                 + "\n"
-                f"[diff]: {risk_diff}",
+                f"[diff]: {diff}",
             )
-            self.last_checked_risk_value = self._just.get_risk_value()
+            self._last_checked_risk_value = self._just.get_risk_value()
 
     def risk_alert_set(self, update: Update, context: CallbackContext):
         chat_id = update.message.chat_id
@@ -70,19 +76,45 @@ class TeleBot:
                 name=str(chat_id) + "_risk_alert_set",
             )
             update.message.reply_text("Risk alert has been set.")
+
+    # Check collateralization ratio of USDD
+    def usdd_cr_check(self, update: Update, context: CallbackContext):
+        update.message.reply_text(f"{round(self._tdr.get_collateralization_ratio() * 100, USDD_CR_DIGIT)}%")
+
+    def _usdd_cr_alert(self, context: CallbackContext):
+        diff = (
+            self._last_checked_usdd_cr_value - self._tdr.get_collateralization_ratio()
+        )
+        diff = round(diff, USDD_CR_DIGIT)
+        # TODO: Calculate minus or plus
+        if USDD_CR_THRESHOLD <= abs(diff):
+            job = context.job
+            context.bot.send_message(
+                job.context,
+                text="USDD CR Alert\n"
+                + f"[{self._last_checked_usdd_cr_value*100}%] -> [{self._tdr.get_collateralization_ratio()*100}%]"
+                + "\n"
+                f"[diff]: {diff}",
+            )
+            self._last_checked_usdd_cr_value = self._tdr.get_collateralization_ratio()
+
+    def usdd_cr_alert_set(self, update: Update, context: CallbackContext):
+        chat_id = update.message.chat_id
+        job_removed = self.remove_job_if_exists(
+            str(chat_id) + "_usdd_cr_alert_set", context
         )
 
         if job_removed:
-            update.message.reply_text("Alert has been reset.")
+            update.message.reply_text("USDD CR alert has been removed.")
         else:
-            self.last_checked_risk_value = self._just.get_risk_value()
-            update.message.reply_text("Alert has been set.")
-
-    def risk_alert_unset(self, update: Update, context: CallbackContext) -> None:
-        chat_id = update.message.chat_id
-        job_removed = self.remove_job_if_exists(str(chat_id) + "_just", context)
-        text = "Unalerted   " if job_removed else "You have no active alert."
-        update.message.reply_text(text)
+            self._last_checked_risk_value = self._just.get_risk_value()
+            context.job_queue.run_repeating(
+                self._usdd_cr_alert,
+                interval=5,
+                context=chat_id,
+                name=str(chat_id) + "_usdd_cr_alert_set",
+            )
+            update.message.reply_text("USDD CR alert has been set.")
 
 
 if __name__ == "__main__":
@@ -97,8 +129,9 @@ if __name__ == "__main__":
     updater.dispatcher.add_handler(CommandHandler("risk_check", bot.risk_check))
     updater.dispatcher.add_handler(CommandHandler("risk_alert", bot.risk_alert_set))
 
+    updater.dispatcher.add_handler(CommandHandler("usdd_cr_check", bot.usdd_cr_check))
     updater.dispatcher.add_handler(
-        CommandHandler("risk_alert_unset", bot.risk_alert_unset)
+        CommandHandler("usdd_cr_alert", bot.usdd_cr_alert_set)
     )
 
     updater.start_polling()
